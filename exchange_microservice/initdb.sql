773b7e6b6acce95c1b418ede6ece3e20790838c0
MHS-20 <muhamad.salman2001@gmail.com>
2023-01-18 19:20:58 +0100

    fixed init db in docker compose

--- exchange_microservice/initdb.sql
+++ exchange_microservice/initdb.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE IF NOT EXISTS exchange;
-USE exchange;
+CREATE DATABASE exchange;
 
 CREATE USER me;
 GRANT ALL PRIVILEGES ON DATABASE exchange TO me;
@@ -8,13 +7,13 @@ GRANT ALL PRIVILEGES ON DATABASE exchange TO me;
 CREATE TABLE IF NOT EXISTS users(
     email VARCHAR(32) PRIMARY KEY NOT NULL,    
     name VARCHAR(32) NOT NULL,
-    iban VARCHAR(100) UNIQUE NOT NULL, 
+    iban VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS wallet(
     email VARCHAR(32) PRIMARY KEY,
-    wallet_eu DOUBLE NOT NULL, 
-    wallet_usd DOUBLE NOT NULL,
+    wallet_eu FLOAT(10) NOT NULL, 
+    wallet_usd FLOAT(10) NOT NULL,
     FOREIGN KEY (email) REFERENCES users(email)  
 );
 
@@ -22,8 +21,8 @@ CREATE TABLE IF NOT EXISTS transactions(
     code SERIAL PRIMARY KEY,
     email VARCHAR(32) NOT NULL,    
     name VARCHAR(32) NOT NULL,
-    eu_amount DOUBLE NOT NULL, 
-    usd_amount DOUBLE NOT NULL,
+    eu_amount FLOAT(10) NOT NULL, 
+    usd_amount FLOAT (10) NOT NULL,
     starting_currency VARCHAR(5) NOT NULL,
     FOREIGN KEY (email) REFERENCES users(email)  
 );
